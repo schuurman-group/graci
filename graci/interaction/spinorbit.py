8a5bd6335792241db55ca1e2bae7effe25a462cf
spneville <sneville.sneville@gmail.com>
2022-02-25 13:10:34 -0500

    1-electron h_soc elements retreived from PySCF/Libcint

--- graci/interaction/spinorbit.py
+++ graci/interaction/spinorbit.py
@@ -5,6 +5,7 @@ elements
 
 import sys as sys
 import numpy as np
+import copy as copy
 import graci.interaction.interaction as interaction
 import graci.utils.timing as timing
 import graci.core.libs as libs
@@ -14,8 +15,10 @@ import graci.io.output as output
 import graci.utils.constants as constants
 
 class Spinorbit(interaction.Interaction):
-    """Spin orbit coupling class. For now will only accept calculations
-       for which the 'Molecule and 'Scf' objects are the same"""
+    """
+    Spin orbit coupling class. For now will only accept calculations
+    for which the 'Molecule and 'Scf' objects are the same
+    """
     def __init__(self):
         # parent attributes
         super().__init__()
@@ -30,24 +33,46 @@ class Spinorbit(interaction.Interaction):
         self.cgcoe      = None
         # Reduced matrix elements
         self.redmat     = None
+        # Bra and ket total spins
+        self.S_bra      = None
+        self.S_ket      = None
+        # Bra and ket projected spins
+        self.M_bra      = None
+        self.M_ket      = None
+        # Bra and ket spin multiplicities
+        self.mult_bra   = None
+        self.mult_ket   = None
+        # One electron SOC matrices
+        self.h1e        = None
+        # SOC Hamiltonian matrix
+        self.hsoc       = None
         
     #
     @timing.timed
     def run(self):
-        """returns the SOC matrix elements between the bra and ket
-           states. If b_state and k_state are None, assume 
-           SOC matrix elements from all states in method object should
-           be used."""
+        """
+        returns the SOC matrix elements between the bra and ket
+        states. If b_state and k_state are None, assume SOC matrix
+        elements from all states in method object should be used
+        """
 
+        # Bra and ket total spins and multiplicities
+        self.mult_bra = self.bra_obj.mult
+        self.mult_ket = self.ket_obj.mult
+        self.S_bra    = (self.mult_bra-1)/2
+        self.S_ket    = (self.mult_ket-1)/2
+
+        # Bra and ket projected spins
+        self.M_bra = [-self.S_bra + i for i in range(self.mult_bra)]
+        self.M_ket = [-self.S_ket + i for i in range(self.mult_ket)]
+        
         # Check on spin multiplicities:
         # (1) Delta S = -1, 0 or +1 must hold
         # (2) In the case of Delta S = 0, only S > 0 must hold
-        S_bra = (self.bra_obj.mult-1)/2
-        S_ket = (self.ket_obj.mult-1)/2
-        if S_bra - S_ket not in [-1., 0., 1.]:
+        if self.S_bra - self.S_ket not in [-1., 0., 1.]:
             sys.exit('\n ERROR: non-sensical S_bra, S_ket combination' \
                      +'in spinorbit')
-        if S_bra == S_ket and S_bra == 0.:
+        if self.S_bra == self.S_ket and self.S_bra == 0.:
             sys.exit('\n ERROR: non-sensical S_bra, S_ket combination ' \
                      +'in spinorbit')
 
@@ -88,12 +113,20 @@ class Spinorbit(interaction.Interaction):
         # get the Clebsh-Gordan coefficients needed to
         # compute the SOC matrix elements
         self.cgcoe = mrci_soc.clebsch_gordan(self.bra_obj, self.ket_obj)
+
+        # get the one-electron SOC matrices
+        self.build_h1e()
+
+        # SOC Hamiltonian matrix elements
+        hsoc = self.build_hsoc()
             
     #
     @timing.timed
     def build_redmat(self):
-        """grab the reduced matrix elements from bitsi and then
-           reshape the list into a more usable format"""
+        """
+        grab the reduced matrix elements from bitsi and then
+        reshape the list into a more usable format
+        """
 
         # grab the reduced matrix elements
         redmat_list = mrci_soc.redmat(self.bra_obj, self.ket_obj,
@@ -114,3 +147,98 @@ class Spinorbit(interaction.Interaction):
             self.redmat[:, :, indx] = redmat_list[birr][kirr][:, :, sym_indx]
 
         del(redmat_list)
+
+
+    #
+    def build_h1e(self):
+        """
+        Sets up the one-electron SOC matrices h^(k), k=-1,0,+1
+        """
+
+        # PySCF Mole obeject
+        mol = self.bra_obj.scf.mol.mol_obj.copy()
+
+        # No. AOs
+        nao = mol.nao_nr()
+        
+        # Initialise arrays
+        self.h1e = np.zeros((3, nao, nao))
+                
+        # One-electron contributions
+        for iatm in range(mol.natm):
+            Z   = mol.atom_charge(iatm)
+            xyz = mol.atom_coord(iatm)
+            mol.set_rinv_orig(xyz)
+            self.h1e += Z * mol.intor('cint1e_prinvxp_sph', 3)
+
+        # Mean-field two-electron contributions
+        
+        
+        # Transform to the MO basis
+
+        
+        return
+        
+    #
+    def hsoc_indx(self, I_bra, I_ket, M_bra, M_ket):
+        """
+        Given bra and ket multiplet indices, I_bra and I_ket,
+        and projected spins, M_bra and M_ket, returns the
+        corresponding element of the SOC Hamiltonian matrix
+        """
+    
+        indx_ket = self.mult_ket * I_ket + M_ket + self.S_ket
+        
+        indx_bra = self.mult_bra * I_bra + M_bra + self.S_bra \
+                   + len(self.init_states) - 2
+        
+        return int(indx_bra), int(indx_ket)
+
+
+    #
+    def build_hsoc(self):
+        """
+        Builds the entire SOC Hamiltonian matrix
+        """
+
+        # Number of bra and ket multiplets
+        nm_bra = len(self.final_states)
+        nm_ket = len(self.init_states)
+
+        # Number of spin-orbit coupled states
+        dim_bra = nm_bra * self.mult_bra
+        dim_ket = nm_ket * self.mult_ket
+
+        # Initialise hsoc (complex matrix)
+        hsoc = np.zeros((dim_bra, dim_ket), dtype=np.cdouble)
+
+        # Loop over pairs of spin-orbit coupled states
+        for I_ket in range(nm_ket):
+            for M_ket in self.M_ket:
+                for I_bra in range(nm_bra):
+                    for M_bra in self.M_bra:
+
+                        # spin-coupled state indices
+                        i, j = self.hsoc_indx(I_bra, I_ket,
+                                              M_bra, M_ket)
+
+                        # H_ij^(SOC)
+                        hsoc[i, j] = self.contract_redmat(I_bra, I_ket,
+                                                          M_bra, M_ket)
+                        
+        return hsoc
+
+    
+    #
+    def contract_redmat(self, I_bra, I_ket, M_bra, M_ket):
+        """
+        Computes a single element 
+        < I_bra M_bra | H_SOC | I_ket M_ket >
+        of the SOC Hamiltonian matrix via the contraction
+        of the reduced matrices with the Clebsch-Gordan coefficient-
+        scaled one-electron SO matrices h^(k), k=-1,0,+1
+        """
+
+        hij = 0.
+        
+        return hij
