87830f2c42b107f793cb5d6b7b3b9ab0bd42db00
simon <sneville.sneville@gmail.com>
2022-06-08 18:09:54 -0400

    Bits and pieces

--- graci/lib/bitci/src/ci/conf_ref/ref_prop.f90
+++ graci/lib/bitci/src/ci/conf_ref/ref_prop.f90
@@ -18,6 +18,8 @@ subroutine ref_space_propagate(nmo0,nmo1,smat,conffile0_in,nconf,&
   use iso_c_binding, only: C_CHAR
   use constants
   use bitglobal
+  use dethash
+  use mrciutils
   use iomod
 
   ! MO basis dimensions
@@ -47,8 +49,16 @@ subroutine ref_space_propagate(nmo0,nmo1,smat,conffile0_in,nconf,&
   integer(is)                        :: scrnum0
   integer(is)                        :: nconf0,n_int_I,nmoI,nmoE
   integer(ib), allocatable           :: conf_r0(:,:,:),sop_r0(:,:,:)
-  !integer(is)                        :: m2c(nmo0),c2m(nmo0)
+  integer(ib), allocatable           :: conf(:,:,:),sop(:,:,:)     
   integer(is), allocatable           :: m2c0(:),c2m0(:)
+
+  ! Hash table
+  type(dhtbl)                        :: h
+  integer(is)                        :: initial_size
+  integer(ib)                        :: key(n_int,2)
+  
+  ! Everything else
+  integer(is)                        :: i,j,k,n,imo0,imo
   
 !----------------------------------------------------------------------
 ! If C bindings are on, then convert the previous geometry
@@ -71,25 +81,73 @@ subroutine ref_space_propagate(nmo0,nmo1,smat,conffile0_in,nconf,&
 !----------------------------------------------------------------------
   allocate(m2c0(nmo0),c2m0(nmo0))
 
-  call read_ref_confs(scrnum0,nconf0,n_int_I,nmoI,nmoE,conf_r0,sop_r0,&
-       m2c0,c2m0)
+  call read_ref_confs(scrnum0,nconf0,n_int_I,nmoI,nmoE,conf_r0,&
+       sop_r0,m2c0,c2m0)
+
+!----------------------------------------------------------------------
+! Put the ref confs into the 'canonical' MO order
+!----------------------------------------------------------------------
+  ! Allocate arrays
+  allocate(conf(n_int,2,nconf0))
+  conf=0_ib
+
+  ! Resize the conf array
+  conf(1:n_int_I,:,:)=conf_r0
+
+  ! Reorder the conf array
+  call reorder_confs(m2c0,conf,nconf0)
+  
+!----------------------------------------------------------------------
+! Initialise the hash table
+!----------------------------------------------------------------------
+  ! Initialisation
+  initial_size=nconf0
+  call h%initialise_table(initial_size)
+  
+!----------------------------------------------------------------------
+! Construct the current geometry ref confs
+!----------------------------------------------------------------------
+  ! Loop over previous geometry ref confs
+  do i=1,nconf0
+
+     ! Set the new conf assuming a one-to-one MO mapping
+
+     print*,''
+
+     key=0_ib
+     do j=1,2
+        do k=1,n_int
+           do n=0,n_bits-1
+              if (btest(conf(k,j,i),n)) then
+                 imo0=n+1+(k-1)*n_bits
+                 print*,imo0
+              endif
+           enddo
+        enddo
+     enddo
+     
+  enddo
+  
+  !print*,''
+  !do i=1,nmo0
+  !   print*,i,maxval(smat(i,:))
+  !enddo
+  !stop
+
+ 
+  !
+  ! To begin with, we can just assume a one-to-one MO mapping
+  ! i.e., MO rotation angles of zero or pi/2
+  !
+  
+  !
+  ! We should stick the generated confs into a hash table
+  ! to avoid duplicates
+  !
 
   !
-  ! Note that we are going to need to put the r0 confs into the
-  ! 'canonical' MO ordering before continuing
-  ! Also, the conf_r0 array is going to have to be re-dimensioned
-  ! to (***n_int***,2,nconf0) before doing this, as its leading
-  ! dimension is currently n_int_I...
+  ! We should check the irreps of the generated confs...
   !
-
-
-  print*,''
-  print*,'Note that we are going to need to put the r0 confs into the'
-  print*,'canonical MO ordering before continuing'
-  print*,'Also, the conf_r0 array is going to have to be re-dimensioned'
-  print*,'to (***n_int***,2,nconf0) before doing this, as its leading'
-  print*,'dimension is currently n_int_I...'
-  STOP
   
   return
   
