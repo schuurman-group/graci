Bug fix in the calculation of the diabatic potential matrix

graci/methods/cimethod.py
@@ -419,7 +419,7 @@ class Cimethod:
 
             # adiabatic potential matrix
             vmat = np.zeros((nstates, nstates), dtype=float)
-            np.fill_diagonal(vmat, self.energies_sym[irr][0:nstates-1])
+            np.fill_diagonal(vmat, self.energies_sym[irr][:nstates])
 
             # diabatic potential matrix
             wmat = np.matmul(self.adt[irr].T,

graci/overlaptools/bdd.py
@@ -58,11 +58,11 @@ def adt(ref_obj, disp_obj):
         # get the overlaps
         Sij = overlap.overlap(ref_obj, disp_obj, disp_obj.smo,
                               pairs, irr, norm_thresh)
-        
+
         # reshape the wave function overlaps into a more
         # useful form
         Sij = np.reshape(Sij, (nstates, nstates))
-        
+
         # transform using the ref ADT matrix
         Sij = np.matmul(adt_ref.T, Sij)
         
@@ -71,7 +71,7 @@ def adt(ref_obj, disp_obj):
         SST      = np.matmul(Sij, Sij.T)
         SST_sqrt = sp.linalg.sqrtm(SST)
         adt      = np.matmul(S_inv, SST_sqrt)
-        
+
         # save the disp ADT matrix
         disp_obj.adt.append(adt)
         