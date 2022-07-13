f1ffa8d463bb0b0f872e3440f6eed20fdf5c32c0
spneville <sneville.sneville@gmail.com>
2022-07-13 15:46:48 -0400

    Start of work on a vibronic coupling Hamiltonian construction class

---
+++ graci/tools/vibham.py
@@ -0,0 +1,21 @@
+"""
+Vibronic coupling Hamiltonian parameterisation class object
+"""
+
+import sys as sys
+import numpy as np
+
+class Vibham:
+    """
+    Class constructor for the Vibham object
+    """
+    def __init__(self):
+        # User-defined input
+        self.scf_label     = None
+        self.postscf_label = None
+
+
+    @timing.timed
+    def run(self):
+
+        return
