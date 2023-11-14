0e15f4378ca6ab323b0c008978ba1147a75ffbfc
spneville <sneville.sneville@gmail.com>
2023-11-14 12:23:20 -0500

    First pass at the orbital rotation

--- graci/dep/overlap/src/dets/detrot.f90
+++ graci/dep/overlap/src/dets/detrot.f90
@@ -18,14 +18,26 @@ contains
 
     use constants
     use global
+    use detfuncs, only: list_from_bitstring,get_nel_string
 
     implicit none
 
     ! Phase factors
     integer(is), allocatable :: phase_alpha(:),phase_beta(:)
 
+    ! Orbital rotation information
+    integer(ib)              :: pB(n_intB),hB(n_intB)
+    integer(ib)              :: pK(n_intK),hK(n_intK)
+    integer(is)              :: npairsB,npairsK
+    integer(is), allocatable :: plistB(:),hlistB(:)
+    integer(is), allocatable :: plistK(:),hlistK(:)
+    
+    ! Orbital transformation matrices
+    real(dp), allocatable    :: UB(:,:),UK(:,:)    
+    
     ! Everything else
-    integer(is)              :: n,iaX,ibX,id
+    integer(is)              :: n,iaX,ibX,id,i,j
+    real(dp), allocatable    :: tmp(:,:)
     
 !----------------------------------------------------------------------
 ! Bra determinants
@@ -35,11 +47,11 @@ contains
     allocate(phase_beta(nbetaB))
     
     ! Rotate the alpha strings and comput the associated phase factors
-    call rotate_string(n_intB,nalphaB,alphaB,phase_alpha)
-
+    call rotate_string(n_intB,nalphaB,alphaB,phase_alpha,pB,hB)
+    
     ! Rotate the beta strings and comput the associated phase factors
-    call rotate_string(n_intB,nbetaB,betaB,phase_beta)
-
+    call rotate_string(n_intB,nbetaB,betaB,phase_beta,pB,hB)
+    
     ! Apply the phase factors to the eigenvectors
     !
     ! Loop over roots
@@ -47,11 +59,11 @@ contains
        
        ! Loop over alpha strings
        do iaX=1,nalphaB
-
+    
           ! Loop over the determinants corresponding to this alpha
           ! string
           do id=offsetB(iaX),offsetB(iaX+1)-1
-
+    
              ! Beta string for this determinant
              ibX=det2betaB(id)
              
@@ -59,9 +71,9 @@ contains
              vecB(id,n)=phase_alpha(iaX)*phase_beta(ibx)*vecB(id,n)
              
           enddo
-       
+          
        enddo
-
+    
     enddo
        
     ! Deallocate arrays
@@ -76,11 +88,11 @@ contains
     allocate(phase_beta(nbetaK))
     
     ! Rotate the alpha strings and comput the associated phase factors
-    call rotate_string(n_intK,nalphaK,alphaK,phase_alpha)
+    call rotate_string(n_intK,nalphaK,alphaK,phase_alpha,pK,hK)
 
     ! Rotate the beta strings and comput the associated phase factors
-    call rotate_string(n_intK,nbetaK,betaK,phase_beta)
-
+    call rotate_string(n_intK,nbetaK,betaK,phase_beta,pK,hK)
+    
     ! Apply the phase factors to the eigenvectors
     !
     ! Loop over roots
@@ -88,21 +100,21 @@ contains
        
        ! Loop over alpha strings
        do iaX=1,nalphaK
-
+    
           ! Loop over the determinants corresponding to this alpha
           ! string
           do id=offsetK(iaX),offsetK(iaX+1)-1
-
+    
              ! Beta string for this determinant
              ibX=det2betaK(id)
              
              ! Apply the phase factors
-             vecK(id,n)=phase_alpha(iaX)*phase_beta(ibx)*vecB(id,n)
+             vecK(id,n)=phase_alpha(iaX)*phase_beta(ibx)*vecK(id,n)
              
           enddo
        
        enddo
-
+    
     enddo
        
     ! Deallocate arrays
@@ -112,10 +124,72 @@ contains
 !----------------------------------------------------------------------
 ! Rotate the MO overlap matrix
 !----------------------------------------------------------------------
-    ! We need the lists of orbital pair indices first...
-    ! i.e., the plist and hlist arrays used in rotate_string
+    ! Get the number of orbital rotation pairs
+    ! N.B. the get_nel_string routine simply counts the no. set bits
+    ! in a string and can be utilised for this purpose
+    call get_nel_string(n_intB,pB,npairsB)
+    call get_nel_string(n_intK,pK,npairsK)
+
+    ! Allocate arrays
+    allocate(plistB(npairsB))
+    allocate(hlistB(npairsB))
+    allocate(plistK(npairsK))
+    allocate(hlistK(npairsK))
     
-    STOP
+    ! Get the lists of creation and annihilation operators
+    call list_from_bitstring(n_intB,pB,plistB,npairsB)
+    call list_from_bitstring(n_intB,hB,hlistB,npairsB)
+    call list_from_bitstring(n_intK,pK,plistK,npairsK)
+    call list_from_bitstring(n_intK,hK,hlistK,npairsK)
+
+    ! Bra rotation matrix
+    allocate(UB(nmoB,nmoB))
+    UB=0.0d0
+
+    do i=1,nmoB
+       UB(i,i)=1.0d0
+    enddo
+
+    do n=1,npairsB
+       i=plistB(n)
+       j=hlistB(n)
+       UB(i,i)=0.0d0
+       UB(j,j)=0.0d0
+       if (i < j) then
+          UB(i,j)=-1.0d0
+          UB(j,i)=1.0d0
+       else
+          UB(i,j)=1.0d0
+          UB(j,i)=-1.0d0
+       endif
+    enddo
+
+    ! Ket rotation matrix
+    allocate(UK(nmoK,nmoK))
+    UK=0.0d0
+
+    do i=1,nmoK
+       UK(i,i)=1.0d0
+    enddo
+
+    do n=1,npairsK
+       i=plistK(n)
+       j=hlistK(n)
+       UK(i,i)=0.0d0
+       UK(j,j)=0.0d0
+       if (i < j) then
+          UK(i,j)=-1.0d0
+          UK(j,i)=1.0d0
+       else
+          UK(i,j)=1.0d0
+          UK(j,i)=-1.0d0
+       endif
+    enddo
+
+    ! Rotate the MO overlap matrix
+    allocate(tmp(nmoB,nmoK))
+    tmp=matmul(smo,UK)
+    smo=matmul(transpose(UB),tmp)
     
     return
     
@@ -126,9 +200,12 @@ contains
 !                         the fixed-occupation orbitals come first in
 !                         all sigma-strings, sigma in {alpha, beta}
 !                         also computes and returns the associated
-!                         phase factors
+!                         phase factors and the pairwise orbital
+!                         rotation information encoded in the creation
+!                         and annihilation operator bit strings
+!                         p and h
 !######################################################################
-  subroutine rotate_string(n_int,nsigma,sigma,phase)
+  subroutine rotate_string(n_int,nsigma,sigma,phase,p,h)
 
     use constants
     use detfuncs, only: list_from_bitstring, exc_degree_string, &
@@ -142,21 +219,24 @@ contains
 
     ! Phase factors
     integer(is), intent(out)   :: phase(nsigma)
+
+    ! Orbital pairs
+    integer(ib), intent(out)   :: p(n_int),h(n_int)
     
     ! Orbital rotation information
     integer(is)                :: nrot
-    integer(ib)                :: p(n_int),h(n_int),p1(n_int),h1(n_int)
+    integer(ib)                :: p1(n_int),h1(n_int)
     integer(is), allocatable   :: plist(:),hlist(:),plist1(:),hlist1(:)
     
     ! Everything else
-    integer(is)                :: n,j,ih,kh,ip,kp
+    integer(is)                :: n,j,ih,kh,ip,kp,i
     integer(ib)                :: string(n_int)
     integer(is)                :: nchange
 
 !----------------------------------------------------------------------
 ! Get the lists of orbital pairs to be rotated
 !----------------------------------------------------------------------
-    call get_pairs(n_int,nsigma,sigma,nrot,plist,hlist)
+    call get_pairs(n_int,nsigma,sigma,nrot,p,h,plist,hlist)
 
     ! If there are no orbital rotations to perform, then return here
     if (nrot == 0) return
@@ -186,16 +266,16 @@ contains
        ! Loop over orbital rotations
        do j=1,nrot
 
-          ! If the hlist(j)'th is not set in the current sigma-string,
-          ! then set it and unset the plist(j)'th bit
-          kh=(hlist(j)-1)/n_bits+1
-          ih=hlist(j)-1-(kh-1)*n_bits
-          if (.not. btest(string(kh),ih)) then
+          ! If the plist(j)'th is not set in the current sigma-string,
+          ! then set it and unset the hlist(j)'th bit
+          kp=(plist(j)-1)/n_bits+1
+          ip=plist(j)-1-(kp-1)*n_bits
+          if (.not. btest(string(kp),ip)) then
              nchange=nchange+1
-             string(kh)=ibset(string(kh),ih)
-             kp=(plist(j)-1)/n_bits+1
-             ip=plist(j)-1-(kp-1)*n_bits
-             string(kp)=ibclr(string(kp),ip)
+             string(kp)=ibset(string(kp),ip)
+             kh=(hlist(j)-1)/n_bits+1
+             ih=hlist(j)-1-(kh-1)*n_bits
+             string(kh)=ibclr(string(kh),ih)
           endif
           
        enddo
@@ -203,6 +283,7 @@ contains
        !
        ! Compute the associated phase factor
        !
+       phase(n)=1
        if (nchange > 0) then
 
           ! Get the lists of creation and annihilation
@@ -212,7 +293,6 @@ contains
           call list_from_bitstring(n_int,h1,hlist1,nrot)
 
           ! Compute the phase factor
-          phase(n)=1
           do j=1,nchange
 
              ! Accumulate the phase factor for this pi/2 rotation
@@ -245,7 +325,7 @@ contains
 !            to change the spin orbital ordering s.t. the fixed-
 !            occupation orbitals come first
 !######################################################################
-  subroutine get_pairs(n_int,nsigma,sigma,nrot,plist,hlist)
+  subroutine get_pairs(n_int,nsigma,sigma,nrot,p,h,plist,hlist)
 
     use constants
     use detfuncs, only: list_from_bitstring, exc_degree_string, &
@@ -259,6 +339,7 @@ contains
 
     ! Orbital indices
     integer(is), intent(out)   :: nrot
+    integer(ib), intent(out)   :: p(n_int),h(n_int)
     integer(is), allocatable   :: plist(:),hlist(:)
     
     ! Bit string encoding of the fixed-occupation spin orbitals
@@ -267,14 +348,12 @@ contains
     ! Number of fixed-occupation spin orbitals
     integer(is)                :: nfixed
 
-    ! Orbital rotation information
-    integer(ib)                :: p(n_int),h(n_int)
-    
     ! Everything else
     integer(is)                :: n,i,k
     integer(ib)                :: string(n_int)
     integer(ib)                :: tstring(n_int)
-
+    integer(ib)                :: tmp(n_int)
+    
 !----------------------------------------------------------------------
 ! Construct the bit string encoding of the fixed-occupation spin
 ! orbitals
@@ -335,11 +414,15 @@ contains
     
     ! Get the bit string encodings of the orbital pairs to be
     ! rotated
-    call exc_string(n_int,tstring,fstring,p,h)
+    call exc_string(n_int,fstring,tstring,p,h)
 
     ! Get the lists of orbital pair indices
-    call list_from_bitstring(n_int,p,plist,nrot)
-    call list_from_bitstring(n_int,h,hlist,nrot)
+    ! Note that list_from_bitstring destroys the input bit string,
+    ! which, in this case, needs to be preserved
+    tmp=p
+    call list_from_bitstring(n_int,tmp,plist,nrot)
+    tmp=h
+    call list_from_bitstring(n_int,tmp,hlist,nrot)
     
     return
     
