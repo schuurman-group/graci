84f530027ec37137f0eca76ca98e013625d28aa9
Michael Schuurman <mschuurm@gmail.com>
2026-05-29 15:22:56 -0400

    added some functional aliases

--- graci/core/functionals.py
+++ graci/core/functionals.py
@@ -4,5 +4,8 @@ XC functional aliases
 
 aliases = {
     'bhlyp' : 'bhandhlyp',
-    'qtp17' : 'hyb_gga_xc_qtp17'
+    'qtp17' : 'hyb_gga_xc_qtp17',
+    'camqtp01' : 'hyb_gga_xc_camqtp01',
+    'camqtp02' : 'hyb_gga_xc_camqtp02',
+    'cqtp25' : '0.4948*SR_HF(0.4153)+1.00*LR_HF(0.4153)+0.5052*ITYH+0.00*B88,VWN5*0.41+LYP*0.59',
 }
